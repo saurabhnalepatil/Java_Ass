698fbb86869e9665be89c0675d089e161cdce12e
SAURABH  NALE <95145125+saurabhnalepatil@users.noreply.github.com>
2022-02-25 22:26:20 +0530

    Add files via upload

---
+++ Assingment 45/Program2.java
@@ -0,0 +1,91 @@
+/*
+Write a program which display all palindrome elements of singly linked 
+list. 
+Function Prototype :
+void DisplayPallindrome( PNODE Head); 
+Input linked list : |11|->|28|->|17|->|414|->|6|->|89| 
+Output : 11 6 414
+*/
+import java.util.*;
+
+class LinkedList
+{
+	Node head; 
+	int size;
+
+	class Node
+	{
+		int data;
+		Node next;
+		Node(int d) {data = d; next = null; }
+	}
+
+	public void InsertLast(int new_data)
+	{
+		Node new_node = new Node(new_data);
+
+		if (head == null)
+		{
+			head = new Node(new_data);
+			return;
+		}
+		new_node.next = null;
+
+		Node last = head;
+		while (last.next != null)
+			last = last.next;
+
+		last.next = new_node;
+		size++;
+		return;
+	}
+	public void printList()
+	{
+		Node tnode = head;
+		while (tnode != null)
+		{
+			System.out.print("|"+tnode.data+"|-> ");
+			tnode = tnode.next;
+		}
+		System.out.println("");
+	}
+	public void RevNo()
+	{
+		Node tnode = head;
+		int iDigit = 0;
+		while(tnode != null)
+		{
+			int temp = tnode.data;
+			while(tnode.data != 0)
+			{
+				iDigit = tnode.data % 10;
+				System.out.print(iDigit);
+				tnode.data = tnode.data / 10;
+			}
+			//System.out.print("|"+temp+"|-> Reverse in no is : "+iMult+"\n");
+			tnode = tnode.next;
+		}	
+	}
+
+	public static void main(String[] args)	
+	{
+		LinkedList obj = new LinkedList();
+
+		Scanner sobj = new Scanner(System.in);
+		System.out.println("Enter number of element in Linkedlist :");
+		int no = sobj.nextInt();
+		System.out.println("Created Linked list is :"+no +"->\n"); 
+		
+		for(int i = 1; i <= no; i++)
+		{
+			System.out.println("Enter the element :"+i +"->");
+			int value = sobj.nextInt();
+			obj.InsertLast(value);
+		}
+		System.out.println("\nCreated Linked list is :");
+		obj.printList();
+
+		System.out.println("\nReverse in Ele. number is LinkedList :");
+		obj.RevNo();	
+	}
+}
---
+++ Assingment 45/Program4.java
@@ -0,0 +1,96 @@
+/*
+Write a program which display smallest digits of all element from singly 
+linear linked list.
+Function Prototype :
+void DisplaySmall( PNODE Head); 
+Input linked list : |11|->|250|->|532|->|415| 
+Output : 1 0 2 1
+*/
+
+import java.util.*;
+
+class LinkedList
+{
+	Node head; 
+	int size;
+
+	class Node
+	{
+		int data;
+		Node next;
+		Node(int d) {data = d; next = null; }
+	}
+
+	public void InsertLast(int new_data)
+	{
+		Node new_node = new Node(new_data);
+
+		if (head == null)
+		{
+			head = new Node(new_data);
+			return;
+		}
+		new_node.next = null;
+
+		Node last = head;
+		while (last.next != null)
+			last = last.next;
+
+		last.next = new_node;
+		size++;
+		return;
+	}
+	public void printList()
+	{
+		Node tnode = head;
+		while (tnode != null)
+		{
+			System.out.print("|"+tnode.data+"|-> ");
+			tnode = tnode.next;
+		}
+		System.out.println("");
+	}
+	public void MultDigit()
+	{
+		Node tnode = head;
+		int iDigit = 0,value = 0;
+		while(tnode != null)
+		{
+			int temp = tnode.data;
+			while(tnode.data != 0)
+			{
+				iDigit = tnode.data % 10;
+				int iMin = iDigit = value;
+				if(iDigit < iMin){
+					iDigit = iMin;
+					iMin = value;
+				}
+				tnode.data = tnode.data / 10;
+			}
+			System.out.print("|"+temp+"|-> Product in Digit is : "+value+"\n");
+			tnode = tnode.next;
+		}	
+	}
+
+	public static void main(String[] args)	
+	{
+		LinkedList obj = new LinkedList();
+
+		Scanner sobj = new Scanner(System.in);
+		System.out.println("Enter number of element in Linkedlist :");
+		int no = sobj.nextInt();
+		System.out.println("Created Linked list is :"+no +"->\n"); 
+		
+		for(int i = 1; i <= no; i++)
+		{
+			System.out.println("Enter the element :"+i +"->");
+			int value = sobj.nextInt();
+			obj.InsertLast(value);
+		}
+		System.out.println("\nCreated Linked list is :");
+		obj.printList();
+
+		System.out.println("\nMultiplication in Ele. Digit is LinkedList :");
+		obj.MultDigit();	
+	}
+}
