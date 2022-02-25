9aac94fe213990244bbad8569232578af1884ea6
SAURABH  NALE <95145125+saurabhnalepatil@users.noreply.github.com>
2022-02-25 22:10:07 +0530

    Add files via upload

---
+++ Assingment 15/Program5.java
@@ -0,0 +1,52 @@
+/*
+Accept number of rows and number of columns from user and display below 
+pattern.
+Input : iRow = 4 iCol = 4
+Output : 
+1 2 3 4 5
+1 2     5
+1   3   5
+1     4 5
+1 2 3 4 5
+Program Layout :
+*/
+
+import java.util.*;
+
+class Program5
+{
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter the Rows number:");
+		int value1 = scan.nextInt();
+
+		System.out.println("Enter the Columns number:");
+		int value2 = scan.nextInt();
+
+		Lucifer obj = new Lucifer();
+		obj.Pattern(value1,value2);
+	}
+}
+
+class Lucifer
+{
+	public void Pattern(int iRow, int iCol)
+	{
+	
+		for(int i = 1; i <= iRow; i++)
+		{
+			for(int j = 1; j <= iCol; j++)
+			{
+				if((i==j)||(i==1)||(i==iRow)||(j==iCol)||(j==1)){
+					System.out.print(" "+j);
+				}
+				else{
+					System.out.print("  ");
+				}
+			}
+			System.out.println(" ");
+		}
+	}
+}
